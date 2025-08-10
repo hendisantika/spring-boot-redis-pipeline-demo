173beb11969269b99d1f82203bdd91cc8b50222a
hendisantika <hendisantika@yahoo.co.id>
2025-08-10 18:51:26 +0700

    feat: Init Project🫸🌀✏️📗🐧🐳

---
+++ src/main/java/id/my/hendisantika/redispipelinedemo/SpringBootRedisPipelineDemoApplication.java
@@ -0,0 +1,13 @@
+package id.my.hendisantika.redispipelinedemo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringBootRedisPipelineDemoApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SpringBootRedisPipelineDemoApplication.class, args);
+    }
+
+}
---
+++ src/test/java/id/my/hendisantika/redispipelinedemo/SpringBootRedisPipelineDemoApplicationTests.java
@@ -0,0 +1,13 @@
+package id.my.hendisantika.redispipelinedemo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringBootRedisPipelineDemoApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
